feaf31193fb7a523f2fd42f08404ad724c07e721
biyu-aja <abiyuaflah135@gmail.com>
2025-11-19 09:25:24 +0700

    Initial

---
+++ front-end/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return(
+    <div className='text-red-500 font-bold'>
+      halo, kalau tulisan ini warna merah berarti tailwind dah jalan
+    </div>
+  )
+}
+
+export default App
